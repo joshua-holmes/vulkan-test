f7745d4835ac2cfe399a8765e5615a8bcffd521e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2023-12-23 09:19:14 -0800

    Cleanup and add comments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use vulkano::{
     instance::{Instance, InstanceCreateInfo},
     device::{QueueFlags, Device, DeviceCreateInfo, QueueCreateInfo},
     memory::allocator::{StandardMemoryAllocator, AllocationCreateInfo, MemoryTypeFilter},
-    buffer::{Buffer, BufferCreateInfo, BufferUsage, BufferContents},
+    buffer::{Buffer, BufferCreateInfo, BufferUsage},
     command_buffer::{
         allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo},
         AutoCommandBufferBuilder,
@@ -15,23 +15,17 @@ use vulkano::{
     sync::{self, GpuFuture}
 };
 
-#[derive(BufferContents)]
-#[repr(C)]
-struct MyStruct {
-    a: u32,
-    b: u32,
-}
-
 fn main() {
+    // setup vulkan
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
     let instance = Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
 
+    // setup device
     let physical_device = instance
         .enumerate_physical_devices()
         .expect("could not enumerate devices")
         .next()
         .expect("no devices available");
-
     let queue_family_index = physical_device
         .queue_family_properties()
         .iter()
@@ -40,7 +34,6 @@ fn main() {
             queue_family_properties.queue_flags.contains(QueueFlags::GRAPHICS)
         })
         .expect("couldn't find a graphical queue fmaily") as u32;
-
     let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo {
@@ -52,11 +45,10 @@ fn main() {
         }
     )
     .expect("failed to create device");
-
     let queue = queues.next().unwrap();
-
     let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
 
+    // setup original buffers
     let source_content: Vec<i32> = (0..64).collect();
     let source = Buffer::from_iter(
         memory_allocator.clone(),
@@ -89,37 +81,33 @@ fn main() {
     )
     .expect("failed to create destination buffer");
 
+    // setup command buffer
     let command_buffer_allocator = StandardCommandBufferAllocator::new(
         device.clone(),
         StandardCommandBufferAllocatorCreateInfo::default(),
     );
-
     let mut builder = AutoCommandBufferBuilder::primary(
         &command_buffer_allocator,
         queue_family_index,
         CommandBufferUsage::OneTimeSubmit,
     )
     .expect("failed to create command buffer");
-
     builder.copy_buffer(
         CopyBufferInfo::buffers(source.clone(), destination.clone())
     ).unwrap();
-
     let command_buffer = builder.build().expect("failed to build command buffer");
 
+    // execute command
     let future = sync::now(device.clone())
         .then_execute(queue.clone(), command_buffer)
         .expect("could not execute order 66")
         .then_signal_fence_and_flush()
         .expect("failed to flush command buffer");
     
+    // wait for command to finish and read data
     future.wait(None).expect("failed to wait for the future");
-
     let future_src = source.read().expect("failed to read future source buffer");
     let future_dst = destination.read().expect("failed to read future destination buffer");
     assert_eq!(&*future_src, &*future_dst);
-
     println!("{:?}\n{:?}", &*future_src, &*future_dst);
-
-
 }
